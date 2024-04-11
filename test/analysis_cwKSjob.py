7852331ae48727c9c88898af346fe204250d8326
Marcin Konecki <marcin.konecki@cern.ch>
2024-04-11 10:20:06 +0200

    submit..

--- test/analysis_cwKSjob.py
+++ test/analysis_cwKSjob.py
@@ -4,38 +4,21 @@ import sys
 import subprocess
 
 process = cms.Process("MojaAnaliza")
+jobId=sys.argv[1]
+flist=[]
+for f in sys.argv[2].split():
+  flist.append('file:'+f.strip('\''))
+print('flist:',flist)
 
 # MessageLogger & co.
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(1)
 process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(False))
 
-dataDir='/scratch_cmsse/konec/data/2023D_ParkingDoubleMuonLowMass/'
-lsCommand='ls -1 '+dataDir+'|grep root'
-print ('command: ',lsCommand)
-dir=subprocess.Popen(lsCommand, stdout=subprocess.PIPE,shell=True,text=True)
-lsOutput=dir.communicate()[0]
-files=[]
-for f in lsOutput.split():
-  print ('file:'+dataDir+f)
-  files.append('file:'+dataDir+f)
-
-print (files)
-print (len(files))
-  
-
-
 # input files (up to 255 files accepted)
-process.source = cms.Source('PoolSource',
-  fileNames = cms.untracked.vstring(
-#   'file:data.root',
-#'root://xrootd-cms.infn.it//store/data/Run2023D/ParkingDoubleMuonLowMass0/MINIAOD/22Sep2023_v1-v1/2550000/0419eec5-0ae4-4732-8f06-6d72dd25a149.root',
- 'root://cms-xrd-global.cern.ch//store/data/Run2023D/ParkingDoubleMuonLowMass0/MINIAOD/22Sep2023_v1-v1/2550000/0419eec5-0ae4-4732-8f06-6d72dd25a149.root',
-  ),
-)
-#process.source.fileNames = files
+process.source = cms.Source('PoolSource', fileNames =cms.untracked.vstring(flist) )
 process.source.skipEvents = cms.untracked.uint32(0)
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100))
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10))
 
 process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
 process.load('Configuration.Geometry.GeometryDB_cff')
@@ -52,7 +35,7 @@ process.options = cms.untracked.PSet( wantSummary=cms.untracked.bool(False))
 
 process.analiza= cms.EDAnalyzer("Analysis",
   muonSrc = cms.InputTag("slimmedDisplacedMuons"),
-  outHist = cms.string("histos.root")
+  outHist = cms.string('histos_'+jobId+'.root')
 )
 
 process.MyPath = cms.Path(process.analiza)
