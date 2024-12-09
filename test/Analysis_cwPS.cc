95df384c6eab24897a8ca526c456a86fb29e543b
Marcin Konecki <marcin.konecki@cern.ch>
2024-12-09 17:47:03 +0100

    cwiczenie dla PS

---
+++ test/Analysis_cwPS.cc
@@ -0,0 +1,130 @@
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "DataFormats/Common/interface/Handle.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "DataFormats/PatCandidates/interface/Muon.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/Candidate/interface/Candidate.h"
+#include "DataFormats/Candidate/interface/VertexCompositePtrCandidate.h"
+
+#include "TrackingTools/TransientTrack/interface/TransientTrackBuilder.h"
+#include "TrackingTools/Records/interface/TransientTrackRecord.h"
+#include "TrackingTools/TransientTrack/interface/TransientTrack.h"
+#include "RecoVertex/VertexPrimitives/interface/TransientVertex.h"
+#include "RecoVertex/KalmanVertexFit/interface/KalmanVertexFitter.h"
+// #include "TrackingTools/IPTools/interface/IPTools.h"
+
+#include "DataFormats/PatCandidates/interface/PackedCandidate.h"
+
+
+#include "TH1D.h"
+#include "TH2D.h"
+#include "TFile.h"
+#include "TMath.h"
+#include <Math/Vector4D.h>
+
+#include <sstream>
+
+using namespace std;
+
+double muonMass = 0.105658;
+double kaonMass = 0.493677;
+double jpsiMass = 3.096900;
+double pionMass = 0.139570;
+double protonMass = 0.938272;
+double lambdaMass = 1.115683;
+double phiMass = 1.019461;
+
+template <typename T> T sqr(T v) { return v*v; }
+
+ROOT::Math::PxPyPzEVector lorentzVector(const math::XYZVector & mom, double mass) {
+  return ROOT::Math::PxPyPzEVector( mom.x(), mom.y(), mom.z(), sqrt( sqr(mass)+mom.mag2()));
+}
+ROOT::Math::PxPyPzEVector lorentzVector(const ROOT::Math::PxPyPzEVector & orig, double mass) {
+  return ROOT::Math::PxPyPzEVector(orig).SetE(sqrt(mass*mass+orig.P2()));
+}
+
+
+//object definition
+class Analysis : public edm::one::EDAnalyzer<> {
+public:
+
+  //constructor, function is called when new object is created
+  explicit Analysis(const edm::ParameterSet& conf);
+
+  //destructor, function is called when object is destroyed
+  ~Analysis();
+
+  //edm filter plugin specific functions
+  virtual void beginJob();
+  virtual void analyze(const edm::Event&, const edm::EventSetup&);
+  virtual void endJob();
+
+private:
+
+  edm::ParameterSet theConfig;
+  bool debug;
+  unsigned int theEventCount;
+  TH1D *histo;
+
+  edm::EDGetTokenT< vector<pat::Muon> > theMuonToken;
+
+};
+
+
+Analysis::Analysis(const edm::ParameterSet& conf)
+  : theConfig(conf), debug(false),  theEventCount(0)
+{
+  cout <<" CTORXX" << endl;
+  theMuonToken = consumes< vector<pat::Muon> >( theConfig.getParameter<edm::InputTag>("muonSrc"));
+  if(theConfig.exists("debug")) debug = theConfig.getParameter<bool>("debug"); 
+}
+
+Analysis::~Analysis()
+{
+  cout <<" DTOR" << endl;
+}
+
+void Analysis::beginJob()
+{
+  //create a histogram
+  histo =new TH1D("histo","test; X; #events",10, 0., 10.);
+  cout << "HERE Analysis::beginJob()" << endl;
+}
+
+void Analysis::endJob()
+{
+  //make a new Root file
+  TFile myRootFile( theConfig.getParameter<std::string>("outHist").c_str(), "RECREATE");
+  //write histogram data
+  histo->Write();
+  myRootFile.Close();
+  delete histo;
+  cout << "HERE Cwiczenie::endJob()" << endl;
+}
+
+void Analysis::analyze(
+    const edm::Event& ev, const edm::EventSetup& es)
+{
+  if (debug) std::cout << " -------------------------------- HERE Cwiczenie::analyze "<< std::endl;
+  const vector<pat::Muon> & muons = ev.get(theMuonToken);
+//  const vector<pat::PackedCandidate> & candidates = ev.get(theCandidateToken);
+
+  if (debug) std::cout <<" number of      muons: " << muons.size() <<std::endl;
+
+  std::vector< std::pair<reco::TransientTrack, reco::TransientTrack> > jpsis;
+  for (std::vector<pat::Muon>::const_iterator im1 = muons.begin(); im1 < muons.end(); im1++) {
+    const pat::Muon & muon = *im1;
+    histo->Fill(muon.eta());
+  }
+
+  if (debug) cout <<"*** Analyze event: " << ev.id()<<" analysed event count:"<<++theEventCount << endl;
+}
+
+DEFINE_FWK_MODULE(Analysis);
+
