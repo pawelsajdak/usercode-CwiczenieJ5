eb9b9459cd5de03c88cc4b446f95aa9b79d21015
pawelsajdak <101072827+pawelsajdak@users.noreply.github.com>
2024-12-30 22:31:18 +0100

    stops at creation of TCanvas

---
+++ test/fitting.C
@@ -0,0 +1,26 @@
+#include "TMath.h"
+#include "TFile.h"
+#include "TCanvas.h"
+
+#include <iostream>
+using namespace std;
+
+// Background
+Double_t background(Double_t *x, Double_t *par){
+    return par[0]+par[1]*x[0]+par[2]*x[0]*x[0];
+}
+
+// Gaussian peak
+Double_t peak(Double_t *x, Double_t *par){
+    return par[0]*TMath::Gaus(x[0],par[1],par[2]);
+}
+
+// Fitting function
+Double_t fitFunction(Double_t *x, Double_t *par){
+    return background(x, par) + peak(x, &par[3]);
+}
+
+void fitting(){
+    cout << "Hello" << endl;
+    new TCanvas;
+}
