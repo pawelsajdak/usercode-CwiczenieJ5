3038efbdde02cff059356f905b6d105fa42176fc
pawelsajdak <101072827+pawelsajdak@users.noreply.github.com>
2025-01-09 22:00:52 +0100

    writes out the results

--- test/newfitting.py
+++ test/newfitting.py
@@ -1,6 +1,13 @@
 import ROOT as r
 import sys
 
+peakname = "J/psi"
+xmin = 2.5
+xmax = 3.5
+par0 = 600.e3
+
+
+
 histfilename = "fullhistogram.root"
 histfile = r.TFile.Open(histfilename,"READ")
 histo = histfile.Get("histo")
@@ -8,9 +15,12 @@ histo.SetDirectory(0)
 histfile.Close()
 
 expression = "[0]*exp((-(x-[1])**2)/(2*[2]**2)) + [3]+x*[4]+x*x*[5]"
-fitFunc = r.TF1("fitFunc",expression,2.5,3.5)
-fitFunc.SetParameters(700000.,3.,0.1,30000.,1.,1.)
-histo.Fit(fitFunc,"ER")
+fitFunc = r.TF1("fitFunc",expression,xmin,xmax)
+fitFunc.SetParameters(par0,(xmax+xmin)/2,0.1,-300000.,200000.,-35000.)
+results = histo.Fit(fitFunc,"ERS")
+
+with open('results.txt', 'a') as of:
+    print(peakname,"\t",fitFunc.GetParameter(1),"\n", results, file=of)
 
 canvas = r.TCanvas("canvas")
 canvas.cd()
