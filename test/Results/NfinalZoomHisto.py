438082b29962f33166d2aa3563f15a44bb8a311a
pawelsajdak <101072827+pawelsajdak@users.noreply.github.com>
2025-02-14 21:27:21 +0100

    14II

---
+++ test/Results/NfinalZoomHisto.py
@@ -0,0 +1,40 @@
+#!/cvmfs/cms.cern.ch/el9_amd64_gcc12/cms/cmssw/CMSSW_14_0_2/external/el9_amd64_gcc12/bin/python3
+import ROOT as r
+import sys
+
+histfilename = "fullhistogram.root"
+histfile = r.TFile.Open(histfilename,"READ")
+histo = histfile.Get("histo")
+histo.SetDirectory(0)
+histfile.Close()
+
+
+canvas = r.TCanvas("canvas")
+canvas.cd()
+canvas.SetBottomMargin(0.12)
+canvas.SetLeftMargin(0.1)
+#canvas.SetLogy(True)
+
+histo.SetAxisRange(0.2, 1.4, "X")
+histo.SetAxisRange(0., 130.e3, "Y")
+histo.SetTitle("Dimuon Events; M_{inv} (GeV); #events")
+histo.SetStats(0)
+r.gStyle.SetTitleFontSize(0.07)
+histo.SetLabelSize(0.04,"XY")
+histo.SetNdivisions(40206, "X")
+histo.GetXaxis().SetTitleSize(0.05)
+histo.GetXaxis().SetTitleOffset(1.0)
+histo.GetYaxis().SetTitleSize(0.05)
+histo.GetYaxis().SetTitleOffset(0.9)
+histo.GetXaxis().CenterTitle(True)
+histo.SetFillColor(19)
+histo.Draw("h")
+l = r.TLatex()
+l.SetTextSize(0.05)
+
+l.DrawLatex(0.53,50.e3,"#eta")
+l.DrawLatex(0.75,115.e3,"#rho,#omega")
+l.DrawLatex(1.,120.e3,"#phi")
+
+canvas.Print("NfinalZoomHisto.pdf")
+input('press enter to exit')
--- test/Results/finalplot.py
+++ test/Results/finalplot.py
@@ -1,3 +1,4 @@
+#!/cvmfs/cms.cern.ch/el9_amd64_gcc12/cms/cmssw/CMSSW_14_0_2/external/el9_amd64_gcc12/bin/python3
 import ROOT as r
 import sys
 
@@ -8,26 +9,34 @@ histo.SetDirectory(0)
 histfile.Close()
 
 
-
-
-
 canvas = r.TCanvas("canvas")
 canvas.cd()
 canvas.SetLogy(True)
+canvas.SetBottomMargin(0.12)
+canvas.SetLeftMargin(0.1)
+
 
 #histo.SetAxisRange(axmin, axmax)
-histo.SetTitle("Dimuon Events; Minv (GeV); #events")
+histo.SetTitle("Dimuon Events; M_{inv} (GeV); #events")
 histo.SetStats(0)
+r.gStyle.SetTitleFontSize(0.07)
+histo.SetLabelSize(0.04,"XY")
+histo.SetNdivisions(212, "X")
 histo.GetXaxis().SetTitleSize(0.05)
+histo.GetXaxis().SetTitleOffset(1.0)
 histo.GetYaxis().SetTitleSize(0.05)
+histo.GetYaxis().SetTitleOffset(0.9)
+histo.GetXaxis().CenterTitle(True)
+histo.SetFillColor(19)
 histo.Draw("h")
 l = r.TLatex()
 l.SetTextSize(0.05)
+l.SetTextFont(42)
 
 l.DrawLatex(2.75,800.e3,"J/#psi")
-l.DrawLatex(3.25,45.e3,"#psi(2S)")
+l.DrawLatex(3.3,45.e3,"#psi(2S)")
 l.DrawLatex(9.,20000.,"#varUpsilon(1,2,3S)")
 l.DrawLatex(0.25,150.e3,"#eta #rho,#omega #phi")
 
-canvas.Print("finalhisto.pdf")
+canvas.Print("Nfinalhisto.pdf")
 input('press enter to exit')
