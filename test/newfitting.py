c7a80054e189d66bbca99d712ceb8b8f75e7fb2e
pawelsajdak <101072827+pawelsajdak@users.noreply.github.com>
2025-02-22 23:11:07 +0100

    work in progress

--- test/newfitting.py
+++ test/newfitting.py
@@ -2,43 +2,47 @@
 import ROOT as r
 import sys
 
-peakname = "XPi"
+peakname = "Xk"
 xmin = 4.0
-xmax = 4.8
-par0 = 3000.
+xmax = 5.0
+par0 = 600.
 #axmin = 3.5
 #axmax = 6.
 
 ##########################################
-histfilename = "myVrtKPP.root"
+histfilename = "JxCorrN.root"
 histfile = r.TFile.Open(histfilename,"READ")
-histo = histfile.Get("histoPi")
+histo = histfile.Get("histoK")
 histo.SetDirectory(0)
 histfile.Close()
-
+'''
+expression = "[0]+x*[1]+x*x*[2]"
+fitFunc = r.TF1("fitFunc",expression,xmin,xmax)
+'''
 expression = "[0]*exp((-(x-[1])**2)/(2*[2]**2)) + [3]+x*[4]+x*x*[5]"
 fitFunc = r.TF1("fitFunc",expression,xmin,xmax)
-fitFunc.SetParameters(par0,(xmin+xmax)/2,0.05,3.e3,1.,1.)
+fitFunc.SetParameters(par0,(xmin+xmax)/2,0.1,45808.7,-20854.5,2406.45)
+
 
 results = histo.Fit(fitFunc,"ERS")
-funcFile = r.TFile.Open("Pifunctions.root","UPDATE")
+funcFile = r.TFile.Open("NKfuncs.root","RECREATE")
 fitFunc.Write("Xfunc")
 #funcFile.Close()
 
-with open('Piresults.txt','a') as of:
+with open('NKresults.txt','a') as of:
     print(peakname,"\t",fitFunc.GetParameter(1),"\n", results, file=of)
 
 canvas = r.TCanvas("canvas")
 canvas.cd()
-#canvas.SetLogy(True)
+canvas.SetLogy(True)
 
 #histo.SetAxisRange(axmin, axmax)
-histo.SetAxisRange(3.5, 6., "X")
-histo.SetAxisRange(2.e3, 7.e3, "Y")
+#histo.SetAxisRange(3.5, 6., "X")
+#histo.SetAxisRange(2.e3, 7.e3, "Y")
 histo.SetTitle(peakname+"\t {:.3f}".format(fitFunc.GetParameter(1))+"; Minv; #events")
 histo.SetStats(0)
 histo.Draw("h")
 
 
-canvas.Print("PiN_"+peakname+".pdf")
+canvas.Print("NK_"+peakname+".pdf")
 input('press enter to exit')
