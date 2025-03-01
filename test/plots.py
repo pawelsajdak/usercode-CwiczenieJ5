97c928b1ed1e6f7c43447ed040a20a783a028139
pawelsajdak <101072827+pawelsajdak@users.noreply.github.com>
2025-03-01 10:06:05 +0100

    psi2SFull

--- test/plots.py
+++ test/plots.py
@@ -6,7 +6,7 @@ from ROOT import *
 
 
 print ("Hello ROOT")
-fileName = "psi2S_test.root"
+fileName = "psi2S.root"
 
 print ('Read data from: ', fileName)
 gROOT.Reset()
@@ -14,11 +14,11 @@ f = TFile(fileName)
 f.ls()
 
 c1 = TCanvas('cHisto','cHisto',600,600)
-#c1.SetLogy(1)
-histo = gROOT.FindObject('histoK')
-histo.SetTitle("psi(2S)+kaon")
-#histo.SetAxisRange(4.4,4.8)
+c1.SetLogy(1)
+histo = gROOT.FindObject('histoPr')
+histo.SetTitle("psi(2S)+proton")
+histo.SetAxisRange(4.6,10.)
 #histo.SetAxisRange(2.e3,8.e3,"Y")
 histo.Draw()
-c1.Print("psi2S_.pdf")
+c1.Print("psi2SFull_Proton.pdf")
 input('press enter to exit')
