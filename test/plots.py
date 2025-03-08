9f430196a17bcdc16888dd27dfd4d31456fb3ea2
pawelsajdak <101072827+pawelsajdak@users.noreply.github.com>
2025-03-08 10:31:41 +0100

    two Candidates plots

--- test/plots.py
+++ test/plots.py
@@ -6,7 +6,7 @@ from ROOT import *
 
 
 print ("Hello ROOT")
-fileName = "JXnewdR.root"
+fileName = "twoCandidates.root"
 
 print ('Read data from: ', fileName)
 gROOT.Reset()
@@ -16,11 +16,11 @@ f.ls()
 c1 = TCanvas('cHisto','cHisto',600,600)
 #c1.SetLogy(1)
 histo = gROOT.FindObject('histoPr')
-histo.SetTitle("J/#psi + #it{p}^{#pm} (#DeltaR > 0.0003); M_{inv};Counts")
+histo.SetTitle("#it{J/#psi} + #it{p}^{+}#it{p}^{-}; M_{inv};Counts")
 histo.GetXaxis().CenterTitle(True)
-histo.SetAxisRange(4.2,7.9)
+histo.SetAxisRange(5.3,12.)
 #histo.SetAxisRange(0.,50.,"Y")
 #histo.SetFillColor(19)
 histo.Draw()
-c1.Print("JPrnewdR.pdf")
+c1.Print("twoPr.pdf")
 input('press enter to exit')
