4f7d0ef34c1499f9e5b58eeedc47fa5729498721
pawelsajdak <101072827+pawelsajdak@users.noreply.github.com>
2025-04-18 17:26:29 +0200

    got Bpm LT from tuple

--- test/newfitting.py
+++ test/newfitting.py
@@ -2,15 +2,15 @@
 import ROOT as r
 import sys
 
-peakname = "BpmTau"
-xmin = 0.002
-xmax = 0.04
+peakname = "18IV"
+xmin = 0.01
+xmax = 0.2
 #par0 = 100.
 #axmin = 3.5
 #axmax = 6.
 
 ##########################################
-histfilename = "BpmLfTm.root"
+histfilename = "BpmLTh.root"
 histfile = r.TFile.Open(histfilename,"READ")
 histo = histfile.Get("histo")
 histo.SetDirectory(0)
@@ -18,7 +18,7 @@ histfile.Close()
 
 #expression = "[0]*exp((-(x-[1])**2)/(2*[2]**2)) + [3]*exp((-(x-[4])**2)/(2*[5]**2))+[6]"
 fitFunc = r.TF1("fitFunc","expo",xmin,xmax)
-fitFunc.SetParameters(6.3,-107.)
+fitFunc.SetParameters(6.5,-28.5)
 
 results = histo.Fit(fitFunc,"ERSLB")
 #funcFile = r.TFile.Open("NPifuncs.root","UPDATE")
@@ -28,20 +28,20 @@ results = histo.Fit(fitFunc,"ERSLB")
 #with open('NPiresults.txt','a') as of:
     #print(peakname,"\t",fitFunc.GetParameter(1),"\n", results, file=of)
 
-lifetime = -1/fitFunc.GetParameter(1)
-print("Lifetime: ",lifetime," +/- ",fitFunc.GetParError(1)*lifetime*lifetime)
+lifetime = (-1/fitFunc.GetParameter(1))/3.e10
+print("Lifetime: ",lifetime," +/- ",3.e10*fitFunc.GetParError(1)*lifetime*lifetime)
 
 canvas = r.TCanvas("canvas")
 canvas.cd()
-canvas.SetLogy(True)
+#canvas.SetLogy(True)
 
 #histo.SetAxisRange(0.0,0.02)
 #histo.SetAxisRange(3.5, 6., "X")
 #histo.SetAxisRange(2.e3, 7.e3, "Y")
-histo.SetTitle("Lifetime of B^{#pm};t;Counts")
+histo.SetTitle("Lifetime of B^{#pm};t [cm/c];Counts")
 #histo.SetStats(0)
 histo.Draw("h")
 
 
-canvas.Print("BpmTauNNLog.pdf")
+canvas.Print("temp.pdf")
 input('press enter to exit')
